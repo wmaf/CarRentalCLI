fa9fd4a388b435c9279ba6303967ef43973951b4
wmaf <wmaf@outlook.com>
2022-02-07 15:52:59 -0500

    test commits

--- src/com/careerdevs/RentalService.java
+++ src/com/careerdevs/RentalService.java
@@ -4,10 +4,25 @@ import java.util.Scanner;
 public class RentalService {
     public static void main(String[] args) {
             System.out.println("Welcome to Bill's Car Rental Program");
+      Car[] carStor = new Car[3];
+
     Car car1 = new Car("Honda", "Accord");
     Car car2 = new Car("Chevy", "Cruze");
     Car car3 = new Car("Toyota","Corolla");
-        System.out.println(car1.getMake()); // test getter
+    // System.out.println(car1.getMake()); // test getter
+
+        carStor[0] = car1;
+        carStor[1] = car2;
+        carStor[2] = car3;
+
+
+        //now you can >>> create an array list to store you objects.
+        // Don't forget to declair the object store'
+
+        for (int i = 0; i < carStor.length; i++) {
+            System.out.println(carStor[i].getMake()+ " "+ carStor[i].getModel());
+
+        }
     }
 }
 
