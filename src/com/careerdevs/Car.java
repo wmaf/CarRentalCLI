1e4e49ee6fecf3e58d39e2d96c346253acfe317f
wmaf <wmaf@outlook.com>
2022-02-07 15:04:40 -0500

    Clean slate - previous code commented at bottom

--- src/com/careerdevs/Car.java
+++ src/com/careerdevs/Car.java
@@ -6,13 +6,62 @@ public class Car {
     private boolean isRented;
 
     //public Car( String make, String model, boolean isRented) {
-    public Car( String make, String model){
+    public Car(String make, String model) {
         this.make = make;
         this.model = model;
         this.isRented = false;
     }
+}
 
-//    public String getMake(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+////    public String getMake(){
+////        return make;
+////    }
+////
+////    public String getModel() {
+////        return model;
+////    }
+////
+////   public String combineName(){
+////        return make +" " + model;
+////    }
+//
+//    public String getMake() {
 //        return make;
 //    }
 //
@@ -20,28 +69,16 @@ public class Car {
 //        return model;
 //    }
 //
-//   public String combineName(){
-//        return make +" " + model;
+//    public boolean isRented() {
+//        return isRented;
 //    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public boolean isRented() {
-        return isRented;
-    }
-
-//    @Override
-//    public String toString() {
-//        return "Car{" +
-//                "make='" + make + '\'' +
-//                ", model='" + model + '\'' +
-//                ", isRented=" + isRented +
-//                '}';
-//  }
-}
+//
+////    @Override
+////    public String toString() {
+////        return "Car{" +
+////                "make='" + make + '\'' +
+////                ", model='" + model + '\'' +
+////                ", isRented=" + isRented +
+////                '}';
+////  }
+//}
--- src/com/careerdevs/RentalService.java
+++ src/com/careerdevs/RentalService.java
@@ -4,21 +4,52 @@ import java.util.Scanner;
 public class RentalService {
     public static void main(String[] args) {
 
-            System.out.println("Welcome to the Car Rental CLI.");
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("what is the make of the car?\nEnter here:");
-            String make = scanner.nextLine();
-
-            System.out.println("What model is the car?\n Enter here:");
-            String model = scanner.nextLine();
-
-            System.out.println("has the car been rented?\n Enter true or false here:");
-            boolean isRented = scanner.nextBoolean();
-
-           // Car userGeneratedRental = new Car(make,model,isRented);
-            Car userGeneratedRental = new Car(make,model);
-            System.out.println(userGeneratedRental);
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// System.out.println("Welcome to the Car Rental CLI.");
+//
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("what is the make of the car?\nEnter here:");
+//         String make = scanner.nextLine();
+//
+//         System.out.println("What model is the car?\n Enter here:");
+//         String model = scanner.nextLine();
+//
+//         System.out.println("has the car been rented?\n Enter true or false here:");
+//         boolean isRented = scanner.nextBoolean();
+//
+//         // Car userGeneratedRental = new Car(make,model,isRented);
+//         Car userGeneratedRental = new Car(make,model);
+//         System.out.println(userGeneratedRental);
